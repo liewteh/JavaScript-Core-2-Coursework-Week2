60a52641f77cb33dfb43ef5cffd07344df842a8d
ltteh <67978926+ltteh@users.noreply.github.com>
2021-01-19 01:01:20 +0000

    List of names

--- exercises/A-list-of-names/script.js
+++ exercises/A-list-of-names/script.js
@@ -1,11 +1,33 @@
 function listOfNames(arrayOfPeople) {
   let content = document.querySelector("#content");
-}
 
-let people = [
-  { name: "Chris", job: "Teacher" },
-  { name: "Joanna", job: "Student" },
-  { name: "Boris", job: "Prime Minister" },
+  // add h1 and h2 tag
+  let createName = document.createElement('h1');
+  let createJob = document.createElement('h2');
+  content.appendChild(createName);
+  content.appendChild(createJob);
+
+  for (let i = 0; i < arrayOfPeople.length; i++) {
+    let listOfNames = document.createTextNode(`${arrayOfPeople[i].name}, `);
+    createName.appendChild(listOfNames);
+
+    let listOfJobs = document.createTextNode(`${arrayOfPeople[i].job}, `);
+    createJob.appendChild(listOfJobs);
+  };
+};
+
+let people = [{
+    name: "Chris",
+    job: "Teacher"
+  },
+  {
+    name: "Joanna",
+    job: "Student"
+  },
+  {
+    name: "Boris",
+    job: "Prime Minister"
+  },
 ];
 
 listOfNames(people);
